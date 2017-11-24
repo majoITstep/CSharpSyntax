992f4c0893e362d771d2de3f7ebcf95751c4d6e9
CSharpFri <CSharpFri@CLASS-2-09>
2017-11-24 21:02:15 +0100

    Add project files.

---
+++ CSharpSyntax/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSyntax
+{
+    class Calculator
+    {
+        public static int Scitaj(int a, int b)
+
+        {
+            int Vysledok = a + b;
+            return Vysledok;
+        }
+        public static int Scitaj(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+          
+        public static int Scitaj(string a, string b)
+        {
+            int mv1 = int.Parse(a);
+            int mv2 = int.Parse(b);
+
+            int Vysledok = 10;
+            switch (Vysledok)
+            {
+                case 2:
+                    Console.WriteLine("Vysledok bol 2");
+                    break;
+
+
+                    if (Vysledok >= 10 && Vysledok <= 20 || Vysledok >= 40 && Vysledok <= 60)
+
+                    {
+                    }
+            }
+        }
+        
+        
+    }
+}
---
+++ CSharpSyntax/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSyntax
+{
+    class Program
+            {
+        public int globalnaPremenna = 7;
+        void TestGlobalnejPremennej()
+        {
+            int lokalnaPremenna = 9;
+            globalnaPremenna++;
+        }
+        static void Main(string[] args)
+        {
+            const int mojaKonstanta = 4;
+            int polomer = 6;
+            //polomer = polomer + 1;
+            polomer+=6 ;
+            Console.WriteLine("Povodny polomer {0}", polomer);
+
+            int novyPolomer = polomer++;
+            Console.WriteLine("Novy polomer {0}", novyPolomer);
+
+            int zaseNovyPolomer = ++polomer;
+            Console.WriteLine("Zase Novy polomer {0}", zaseNovyPolomer);
+
+            Calculator.Scitaj(5, 2);
+            Console.ReadLine();
+        }
+    }
+    }
+
